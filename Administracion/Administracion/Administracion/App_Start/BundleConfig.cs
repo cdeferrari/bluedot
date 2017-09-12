8e7b6002f9b2bfb494a64e71fd96183e08031ad5
Leandro Anacondio <leandro.anacondio@gmail.com>
2017-09-12 19:01:12 -0300

    home estilos basicos

--- Administracion/Administracion/Administracion/App_Start/BundleConfig.cs
+++ Administracion/Administracion/Administracion/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Administracion
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
