aff9ce547d52add8fe3a3965c4f671684fd6e808
lanacondio <leandro.anacondio@gmail.com>
2017-09-26 00:31:23 -0300

    address

---
+++ ApiCore/ApiCore/ApiCore.Repository/Mappings/AddressMap.cs
@@ -0,0 +1,22 @@
+﻿
+using ApiCore.DomainModel;
+
+namespace ApiCore.Repository.Mappings
+{
+    public class AddressMap : EntityMap<Address>
+    {
+        public AddressMap() : base("direccion")
+        {
+            
+            this.Property(x => x.Street).IsRequired().HasColumnName("street");
+            this.Property(x => x.Number).IsRequired().HasColumnName("number");
+            
+            this.Property(x => x.Lat).IsOptional();
+            this.Property(x => x.Len).IsOptional();
+
+
+        }
+
+    }
+}
+
