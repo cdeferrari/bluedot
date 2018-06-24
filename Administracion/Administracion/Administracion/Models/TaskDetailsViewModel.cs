29f2be3ae19a37ac440556dd4e441392c56408d5
Tobias Vidal <tobiasvidald@gmail.com>
2018-06-24 20:06:22 -0300

    Se agrega funcionalidad a Task Details
    Se elimina "_CreateTaskFollow" de Backlog y se lo pasa a Ticket como "AgregarSeguimiento"

--- Administracion/Administracion/Administracion/Models/TaskDetailsViewModel.cs
+++ Administracion/Administracion/Administracion/Models/TaskDetailsViewModel.cs
@@ -8,5 +8,7 @@ namespace Administracion.Models
     public class TaskDetailsViewModel
     {
         public DomainModel.Task Task { get; set; }
+        public List<DomainModel.SpendItem> SpendItemList { get; set; }
+        public int SpendItemId { get; set; }
     }
 }
