58819388002e2c951e23e541db6e1b0d4d588aba
Tobias Vidal <tobiasvidald@gmail.com>
2018-07-31 22:54:53 -0300

    Calendar events update
    Correjido error con mes de los eventos, ahora hacer click lleva al ticket, y el hover muestra el titulo entero.

--- Administracion/Administracion/Administracion/Models/Calendar/Calendar.cs
+++ Administracion/Administracion/Administracion/Models/Calendar/Calendar.cs
@@ -17,6 +17,6 @@ namespace Administracion.Models.Calendar
         public int Month { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
-        public string Url { get; set; }
+        public string Url { get; set; } = "";
     }
 }
